97c850a7a9e2de2d3eb607980292990e43e3ce19
nooksit <57735979+nooksit@users.noreply.github.com>
2019-12-18 09:26:17 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  
+ 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://newagent-pubpjh.firebaseio.com"
+});
+
+
+  function writeToDb (agent) {
+    // Get parameter from Dialogflow with the string to add to the database
+    const databaseEntry = agent.parameters.databaseEntry;
+
+    // Get the database collection 'dialogflow' and document 'agent' and store
+    // the document  {entry: "<value of database entry>"} in the 'agent' document
+    const dialogflowAgentRef = db.collection('dialogflow').doc('agent');
+    return db.runTransaction(t => {
+      t.set(dialogflowAgentRef, {entry: databaseEntry});
+      return Promise.resolve('Write complete');
+    }).then(doc => {
+      agent.add(`Wrote "${databaseEntry}" to the Firestore database.`);
+    }).catch(err => {
+      console.log(`Error writing to Firestore: ${err}`);
+      agent.add(`Failed to write "${databaseEntry}" to the Firestore database.`);
+    });
+  }
+
+  function readFromDb (agent) {
+    // Get the database collection 'dialogflow' and document 'agent'
+    const dialogflowAgentDoc = db.collection('dialogflow').doc('agent');
+
+    // Get the value of 'entry' in the document and send it to the user
+    return dialogflowAgentDoc.get()
+      .then(doc => {
+        if (!doc.exists) {
+          agent.add('No data found in the database!');
+        } else {
+          agent.add(doc.data().entry);
+        }
+        return Promise.resolve('Read complete');
+      }).catch(() => {
+        agent.add('Error reading entry from the Firestore database.');
+        agent.add('Please add a entry to the database first by saying, "Write <your phrase> to the database"');
+      });
+  }
+function welcome(agent) {
+    agent.add(`Hello!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+  // Map from Dialogflow intent names to functions to be run when the intent is matched
+  let intentMap = new Map();
+  intentMap.set('ReadFromFirestore', readFromDb);
+  intentMap.set('WriteToFirestore', writeToDb);
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  agent.handleRequest(intentMap);
+});
